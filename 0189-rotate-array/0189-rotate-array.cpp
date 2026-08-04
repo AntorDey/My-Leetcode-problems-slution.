333f24db7ec3bc25530be39c191dcce7bfef0bd3
Antor Dey <antordey2005@gmail.com>
2026-08-04 21:04:13 +0600

    Time: 0 ms (100%), Space: 30.5 MB (18.67%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        k = k % n; // k, n theke boro hole eta dorkar
+//         int n = nums.size();
+//         k = k % n; // k, n theke boro hole eta dorkar
 
-        vector<int> temp(n);
+//         vector<int> temp(n);
 
-        // notun position e bosao: (i+k)%n
-        for (int i = 0; i < n; i++) {
-            temp[(i + k) % n] = nums[i];
-        }
+//         // notun position e bosao: (i+k)%n
+//         for (int i = 0; i < n; i++) {
+//             temp[(i + k) % n] = nums[i];
+//         }
 
-        nums = temp; // original array update kore dilam
+//         nums = temp; // original array update kore dilam
+//     }
+// };
+
+vector<int>temp(nums.size());
+for(int i = 0; i <nums.size();i++){
+    temp[(i+k)%nums.size()]=nums[i];
+
+}
+nums=temp;
     }
 };
-
