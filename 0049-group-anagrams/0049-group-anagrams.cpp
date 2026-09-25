25d2b9d464576c4ad8cd7ccaa020ae0d4b5e9fd6
Antor Dey <antordey2005@gmail.com>
2026-09-26 01:21:16 +0600

    Time: 15 ms (69.33%), Space: 25.1 MB (64.17%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,23 +1,42 @@
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//         map<string, vector<string>> mp;
+
+//         for (int i = 0; i < strs.size(); i++) {
+//             string s = strs[i];
+//             string temp = s;
+
+//             sort(temp.begin(), temp.end()); // sort string
+
+//             mp[temp].push_back(s); // original string store
+//         }
+
+//         vector<vector<string>> ans;
+
+//         for (auto it : mp) {
+//             ans.push_back(it.second);
+//         }
+
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<string, vector<string>> mp;
+        unordered_map<string, vector<string>> groups;
 
-        for (int i = 0; i < strs.size(); i++) {
-            string s = strs[i];
-            string temp = s;
-
-            sort(temp.begin(), temp.end()); // sort string
-
-            mp[temp].push_back(s); // original string store
+        for (string& s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());  // sorted version acts as the "signature"
+            groups[key].push_back(s);      // add original word to its group
         }
 
-        vector<vector<string>> ans;
-
-        for (auto it : mp) {
-            ans.push_back(it.second);
+        vector<vector<string>> result;
+        for (auto& pair : groups) {
+            result.push_back(pair.second);
         }
 
-        return ans;
+        return result;
     }
 };
